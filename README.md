c6b47d2c9fdc08b4ba37d30e68a2e39123d259a3
Susanna Värtinen <susanna.vartinen@aalto.fi>
2022-07-11 16:21:27 +0300

    focus this repo on code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# rpg-quest-data-set
+# Code for the paper 'Generating Role-Playing Game Quests With GPT Language Models'
 
-A collection of quests from Baldur's Gate, Baldur's Gate II, The Elder Scrolls IV: Oblivion, The Elder Scrolls V: Skyrim, Minecraft, and Torchlight II. The quests from BG, BGII, TES IV, and TES V were pieced together from dialogue files extracted with modding tools. The Minecraft quests were written manually by us. The quests from TLII were originally collected by van Stegeren and Theune ([repo](https://github.com/hmi-utwente/video-game-text-corpora)) and edited by us. 
+This repository contains the code used for our research paper about using a language model to write role-playing game quest descriptions.
+
+A copy of our quest data set is also included in this repository. It consits of quests from Baldur's Gate, Baldur's Gate II, The Elder Scrolls IV: Oblivion, The Elder Scrolls V: Skyrim, Minecraft, and Torchlight II. 
+The quests from BG, BGII, TES IV, and TES V were pieced together from dialogue files extracted with modding tools. The Minecraft quests were written manually by us. The quests from TLII were originally collected by van Stegeren and Theune ([repo](https://github.com/hmi-utwente/video-game-text-corpora)) and edited by us. 
--- data_sets/metrics.py
+++ data_sets/metrics.py
@@ -1,4 +1,5 @@
 import json
+import statistics
 
 import spacy
 import textdescriptives as td
@@ -11,49 +12,44 @@ files = ["BG/quests_BG.json", "BG/quests_BGII.json", "MC/quests_MC.json", "TES/q
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('textdescriptives')
 
-
-total_readability = 0
-total_complexity = 0
-total_richness = 0
-total_words = 0
-total_quests = 0
+total_readability = []
+total_complexity = []
+total_richness = []
+total_words = []
 for file in files:
     f = open(file)
     quest_data = json.load(f)
     
-    readability = 0
-    complexity = 0
-    richness = 0
-    words = 0
+    readability = []
+    complexity = []
+    richness = []
+    words = []
     for quest in quest_data:
         quest_description = quest['description']
         
         doc = nlp(quest_description)
-        readability += doc._.readability['flesch_kincaid_grade']
-        complexity += doc._.dependency_distance['dependency_distance_mean']
+        readability.append(doc._.readability['flesch_kincaid_grade'])
+        complexity.append(doc._.dependency_distance['dependency_distance_mean'])
         
         lex = LexicalRichness(quest_description)
-        richness += lex.ttr
-        words += lex.words
-        
-    quests = len(quest_data)
-    
-    total_quests += quests
-    total_readability += readability
-    total_complexity += complexity
-    total_richness += richness
-    total_words += words
+        richness.append(lex.ttr)
+        words.append(lex.words)
+
+    total_readability.extend(readability)
+    total_complexity.extend(complexity)
+    total_richness.extend(richness)
+    total_words.extend(words)
     
     print("Quest file: " + file)    
-    print("Avg. readability (Flesch-Kincaid grade): " + str(readability / quests))
-    print("Avg. syntactics complexity (dependency distance): " + str(complexity / quests))
-    print("Avg. lexical richness (Type-Token Ratio): " + str(richness / quests))
-    print("Avg. words: " + str(words / quests))
+    print("Avg. readability (Flesch-Kincaid grade): " + str(statistics.mean(readability)) + " (stdev: " + str(statistics.stdev(readability)) + ")")
+    print("Avg. syntactics complexity (dependency distance): " + str(statistics.mean(complexity))  + " (stdev: " + str(statistics.stdev(complexity)) + ")")
+    print("Avg. lexical richness (Type-Token Ratio): " + str(statistics.mean(richness))  + " (stdev: " + str(statistics.stdev(richness)) + ")")
+    print("Avg. words: " + str(statistics.mean(words)) +  " (stdev: " + str(statistics.stdev(words)) + ")")
     print(".........")
     
 print("Overall:")    
-print("Avg. readability (Flesch-Kincaid grade): " + str(total_readability / total_quests))
-print("Avg. syntactics complexity (dependency distance): " + str(total_complexity / total_quests))
-print("Avg. lexical richness (Type-Token Ratio): " + str(total_richness / total_quests))
-print("Avg. words: " + str(total_words / total_quests))
+print("Avg. readability (Flesch-Kincaid grade): " + str(statistics.mean(total_readability))  + " (stdev: " + str(statistics.stdev(total_readability)) + ")")
+print("Avg. syntactics complexity (dependency distance): " + str(statistics.mean(total_complexity))  + " (stdev: " + str(statistics.stdev(total_complexity)) + ")")
+print("Avg. lexical richness (Type-Token Ratio): " + str(statistics.mean(total_richness))  + " (stdev: " + str(statistics.stdev(total_richness)) + ")")
+print("Avg. words: " + str(statistics.mean(total_words))  + " (stdev: " + str(statistics.stdev(total_words)) + ")")
 print(".........")
